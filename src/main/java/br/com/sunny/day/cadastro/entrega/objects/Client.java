ad80b0ba800feb61ca319ebe15dc2fe346b26bc2
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-05-19 00:53:46 -0300

    Criando objetos para persistencia

---
+++ src/main/java/br/com/sunny/day/cadastro/entrega/objects/Client.java
@@ -0,0 +1,73 @@
+package br.com.sunny.day.cadastro.entrega.objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "client")
+public class Client {
+    private Integer id;
+    private String name;
+    private String user;
+    private String password;
+    private String cep;
+    private GenderEnum gender;
+    private LocalDate birthDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public GenderEnum getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderEnum gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
