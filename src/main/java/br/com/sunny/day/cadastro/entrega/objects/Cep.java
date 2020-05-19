e18b895d7f855b5db55847c6b7954ce380c3ba27
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-05-19 01:31:13 -0300

    adicionando consulta de cep

---
+++ src/main/java/br/com/sunny/day/cadastro/entrega/objects/Cep.java
@@ -0,0 +1,49 @@
+package br.com.sunny.day.cadastro.entrega.objects;
+
+public class Cep {
+    private String logradouro;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private Boolean realizaEntrega;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Boolean getRealizaEntrega() {
+        return realizaEntrega;
+    }
+
+    public void setRealizaEntrega(Boolean realizaEntrega) {
+        this.realizaEntrega = realizaEntrega;
+    }
+}
